45a00d96648d77e55a8d8e9533a0071641bd935c
Andrei Oprea <andreidei0406@yahoo.com>
2023-12-04 13:18:52 +0200

    added skeleton for better managing data on the client side

--- dropbox2.0/app/dashboard/page.tsx
+++ dropbox2.0/app/dashboard/page.tsx
@@ -1,13 +1,32 @@
-import React from "react";
 import { auth } from "@clerk/nextjs";
 import Dropzone from "@/components/Dropzone";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/firebase";
+import { FileType } from "@/typings";
 
-function Dashboard() {
+async function Dashboard() {
   const { userId } = auth();
 
-  return <div>
-    <Dropzone/>
-  </div>;
+  const docResults = await getDocs(collection(db, "users", userId!, "files"));
+
+  const skeletonFiles: FileType[] = docResults.docs.map((doc) => ({
+    id: doc.id,
+    filename: doc.data().filename || doc.id,
+    timestamp: new Date(doc.data().timestamp?.seconds * 1000) || undefined,
+    fullName: doc.data().fullName,
+    downloadURL: doc.data().downloadURL,
+    type: doc.data().type,
+    size: doc.data().size,
+  }));
+
+  console.log(skeletonFiles);
+
+  return (
+    <div>
+      <Dropzone />
+      <section></section>
+    </div>
+  );
 }
 
 export default Dashboard;
